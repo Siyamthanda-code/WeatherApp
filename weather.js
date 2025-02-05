3fc8a3b34b6797dbeacb161af31ec47155677ef2
Siyamthanda <siyamthanda917@gmail.com>
2025-02-05 04:43:41 +0200

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,59 @@
+const apiKey = '27e67cd2774dd8a7ccf7da496bb96f74';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+  
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+async function getWeatherByLocation(city){
+     try{
+         const resp = await fetch(url(city));
+         if (!resp.ok){
+            throw new Error ('City not found');
+         }
+         const respData = await resp.json();
+     console.log(respData);
+           addWeatherToPage(respData);
+        }catch (error) {
+            console.error(error);
+            main.innerHTML = `<p>${error.message}</p>`;
+        }
+     }
+
+      function addWeatherToPage(data){
+          const temp = Ktoc(data.main.temp);
+
+          const weather = document.createElement('div')
+          weather.classList.add('weather');
+
+          weather.innerHTML = `
+          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+          <small>${data.weather[0].main}</small>
+          
+          `;
+
+
+        //   cleanup 
+          main.innerHTML= "";
+           main.appendChild(weather);
+      };
+
+
+     function Ktoc(K){
+         return Math.floor(K - 273.15);
+     }
+
+
+
+     form.addEventListener('submit',(e) =>{
+        e.preventDefault();
+
+        const city = search.value;
+
+        if(city){
+            getWeatherByLocation(city)
+        }
+
+     });
